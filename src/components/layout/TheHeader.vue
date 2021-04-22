77a8a4228d1a21f07f605c5f3180039f978ee9b0
Rado Randriamahandry <rado.randriamahandry@gmail.com>
2021-04-22 13:15:42 +0300

    Made the headear responsive

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,29 +1,34 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <Disclosure as="nav" class="bg-white" v-slot="{ open }">
-    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-2 sm:px-4 md:px-6 lg:px-8">
       <!-- <div class="relative flex items-center justify-between h-16"> -->
-      <div class="relative flex justify-between h-16">
+      <div
+        class="relative flex justify-between h-16 sm:h-24 sm:justify-around md:justify-between sm:flex-col md:flex-row md:h-16"
+      >
+        <!-- Mobile menu button-->
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
           <DisclosureButton
-            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-primary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           >
             <span class="sr-only">Open main menu</span>
             <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
             <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
           </DisclosureButton>
         </div>
+
+        <!-- Desktop menu -->
         <div
-          class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start"
+          class="flex-1 sm:flex-initial md:flex-1 flex items-center sm:justify-center md:flex-row md:items-stretch md:justify-start"
         >
-          <div class="flex-shrink-0 flex items-center">
+          <div
+            class="flex-shrink-0 ml-12 sm:ml-0 flex items-center sm:justify-center"
+          >
             <p class="text-gray-600 font-bold uppercase">Your Anime</p>
           </div>
 
-          <!-- Desktop menu -->
           <div class="hidden sm:flex sm:ml-6">
-            <div class="flex h-full space-x-4">
+            <div class="flex h-full sm:space-x-0 md:space-x-4">
               <a
                 v-for="item in navigation"
                 :key="item.name"
@@ -42,7 +47,9 @@
         </div>
 
         <!-- Right part -->
-        <SearchBar />
+        <div class="sm:flex sm:justify-center md:flex-none">
+          <SearchBar />
+        </div>
         <!-- <div
           class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
         > -->
@@ -122,16 +129,16 @@
 
     <!-- Mobile menu -->
     <DisclosurePanel class="sm:hidden">
-      <div class="px-2 pt-2 pb-3 space-y-1">
+      <div class="px-2 pt-2 pb-3 space-y-1 shadow-sm">
         <a
           v-for="item in navigation"
           :key="item.name"
           :href="item.href"
+          class="block px-3 py-2 text-base font-medium"
           :class="[
             item.current
-              ? 'bg-gray-900 text-white'
-              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-            'block px-3 py-2 rounded-md text-base font-medium',
+              ? 'bg-primary text-white'
+              : 'text-gray-500 hover:bg-gray-300 hover:text-gray-700',
           ]"
           :aria-current="item.current ? 'page' : undefined"
           >{{ item.name }}</a
@@ -144,15 +151,7 @@
 <script>
 import { ref } from "vue";
 import SearchBar from "@/components/SearchBar";
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  // Menu,
-  // MenuButton,
-  // MenuItem,
-  // MenuItems,
-} from "@headlessui/vue";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import { MenuIcon, XIcon } from "@heroicons/vue/outline";
 
 export default {
@@ -161,11 +160,7 @@ export default {
     Disclosure,
     DisclosureButton,
     DisclosurePanel,
-    // Menu,
-    // MenuButton,
-    // MenuItem,
-    // MenuItems,
-    // BellIcon,
+
     MenuIcon,
     XIcon,
   },
